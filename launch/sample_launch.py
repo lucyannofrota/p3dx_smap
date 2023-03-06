23066606bf55b2952a39d62de7303a857ee17d17
Lucyanno Frota <lucyannofrota@gmail.com>
2023-03-06 18:22:49 +0000

    Initial commit

---
+++ launch/sample_launch.py
@@ -0,0 +1,26 @@
+import launch
+from launch.actions import TimerAction
+import launch_ros.actions
+
+from classifier_importer.classifier_importer import cimporter
+
+def generate_launch_description():
+
+    # Classifier importer
+    classifiers = []
+    classifiers.append({
+        "import_string": "from smap_classifiers.classifier_1 import classifier_1",
+        "class_name": "classifier_1",
+        "args": "\'yolov1\'"
+    })
+    classifiers.append({
+        "import_string": "from smap_classifiers.classifier_2 import classifier_2",
+        "class_name": "classifier_2",
+        "args": "\'yolov2\'"
+    })
+
+    cimporter(classifiers)
+
+    return launch.LaunchDescription([
+        #im
+    ])
